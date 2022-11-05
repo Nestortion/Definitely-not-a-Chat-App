c6ab860ee372c789b47006941575a37c75f39376
Josel Catalan <catalanjosel123@gmail.com>
2022-11-05 13:19:44 +0800

    refactor: remove unnecessary styling on #next

--- client/components/styles/GlobalStyle.jsx
+++ client/components/styles/GlobalStyle.jsx
@@ -10,16 +10,7 @@ const GlobalStyle = createGlobalStyle`
         background-color: ${({ theme }) => theme.colors.backGround};
         font-family: ${({ theme }) => theme.font.fontFamily};
         color: ${({ theme }) => theme.colors.foreGround};
-    
-    #__next{
-        height: 100%;
-        width: 100%;
-        display: flex;
-        flex-direction: column;
     }
-    }
-    
-    
 `
 
 export default GlobalStyle
