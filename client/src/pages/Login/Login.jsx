60020fbd8e74b96d756f2178407274602df33e27
Josel Catalan <catalanjosel123@gmail.com>
2022-11-17 19:09:27 +0800

    chore: finish Login page

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,3 +1,28 @@
+import styles from './Login.scss'
+import Input from '../../components/UI/Input/Input'
+import Button from '../../components/UI/Button/Button'
+
 export default function Login() {
-  return <div>Login</div>
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate
+            possimus tempora molestias autem nisi doloremque ipsam labore dolor
+            rerum nihil!
+          </p>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <Input id="username" type="text" placeholder="Username" />
+            <Input id="password" type="password" placeholder="Password" />
+            <Button>Submit</Button>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
 }
