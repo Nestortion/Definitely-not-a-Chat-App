9189538d1538d83eb46790587da0e2aaba65a902
Josel Catalan <catalanjosel123@gmail.com>
2022-11-17 19:33:44 +0800

    chore: add Layout styling

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -2,16 +2,23 @@ import { Outlet } from 'react-router-dom'
 import LeftBar from '../LeftBar/LeftBar'
 import NavBar from '../NavBar/NavBar'
 import RightBar from '../RightBar/RightBar'
+import './layout.scss'
 
 export default function Layout() {
   return (
-    <>
+    <div className="layout">
       <NavBar />
-      <div>
-        <LeftBar />
-        <Outlet />
-        <RightBar />
+      <div className="main">
+        <div className="left">
+          <LeftBar />
+        </div>
+        <div className="middle">
+          <Outlet />
+        </div>
+        <div className="right">
+          <RightBar />
+        </div>
       </div>
-    </>
+    </div>
   )
 }
