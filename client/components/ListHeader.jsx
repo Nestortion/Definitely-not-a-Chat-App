0b0064e069479ed177026c7772af9ea94ba0e358
Josel Catalan <catalanjosel123@gmail.com>
2022-11-17 13:41:54 +0800

    chore: remove all client files

--- client/components/ListHeader.jsx
+++
@@ -1,5 +0,0 @@
-import { ListHeaderStyled } from './styles/UserGroupListStyled'
-
-export default function ListHeader() {
-  return <ListHeaderStyled>ListHeader</ListHeaderStyled>
-}
--- client/components/MainChat/ChatFeed.jsx
+++
@@ -1,5 +0,0 @@
-import { ChatFeedStyled } from '../styles/MainChatStyled'
-
-export default function ChatFeed() {
-  return <ChatFeedStyled>ChatFeed</ChatFeedStyled>
-}
