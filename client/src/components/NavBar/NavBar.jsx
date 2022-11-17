620111f0c18558c499ef1b14d8949a613323a4d2
Josel Catalan <catalanjosel123@gmail.com>
2022-11-17 19:20:45 +0800

    chore: add NavBar styling

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,3 +1,18 @@
+import './navbar.scss'
+import { Link } from 'react-router-dom'
+
 export default function NavBar() {
-  return <div>NavBar</div>
+  return (
+    <div className="navbar">
+      <div className="left">
+        <Link to="/">
+          <span>DNCA</span>
+        </Link>
+      </div>
+      <div className="right">
+        <div>User Image</div>
+        <div>John Doe</div>
+      </div>
+    </div>
+  )
 }
