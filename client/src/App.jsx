ccd964ec099a6946134d1a80e9169773d6a7fc3f
Josel Catalan <catalanjosel123@gmail.com>
2022-11-17 18:24:15 +0800

    chore: implement routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
+import Layout from './components/Layout/Layout'
+
 export default function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Layout />
     </>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Login from './pages/Login/Login'
+import NotFound from './pages/NotFound/NotFound'
+import Home from './pages/Home/Home'
+import Chat from './pages/Chat/Chat'
+import Profile from './pages/Profile/Profile'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/chat/:chatId',
+        element: <Chat />,
+      },
+      {
+        path: '/profile/:profileId',
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
