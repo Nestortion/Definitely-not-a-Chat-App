c30546ef28eb4fcbcaa5c9e450ffb964ed1ae73f
Josel Catalan <catalanjosel123@gmail.com>
2022-11-18 15:54:17 +0800

    chore: create Chat page style

--- client/src/pages/Chat/Chat.jsx
+++ client/src/pages/Chat/Chat.jsx
@@ -1,3 +1,31 @@
+import './chat.scss'
+import Button from '../../components/UI/Button/Button'
+import Input from '../../components/UI/Input/Input'
+
 export default function Chat() {
-  return <div>Chat</div>
+  return (
+    <div className="chat">
+      <div className="header">
+        <div className="chat-info">
+          <div>
+            <span>Chat image</span>
+            <span>Chat title</span>
+          </div>
+        </div>
+        <Button>+</Button>
+      </div>
+      <div className="chat-messages-container">
+        <div>Chat 1</div>
+        <div>Chat 2</div>
+      </div>
+      <div className="chat-input-container">
+        <form>
+          {/* TODO: Create FileInput component */}
+          <button>+</button>
+          <Input type="text" />
+          <Button>Send</Button>
+        </form>
+      </div>
+    </div>
+  )
 }
