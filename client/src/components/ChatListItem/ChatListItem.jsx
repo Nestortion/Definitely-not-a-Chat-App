a9e6669b8a94570767c3262f2805d9634ba1c846
Josel Catalan <catalanjosel123@gmail.com>
2022-11-18 19:10:45 +0800

    chore: implement navigate when ChatListItem is clicked

--- client/src/components/ChatListItem/ChatListItem.jsx
+++ client/src/components/ChatListItem/ChatListItem.jsx
@@ -1,9 +1,16 @@
 import './chat-list-item.scss'
 import Avatar from '../UI/Avatar/Avatar'
+import { useNavigate } from 'react-router-dom'
 
 export default function ChatListItem({ chatId, title, profilePicUrl }) {
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    navigate(`/chat/${chatId}`)
+  }
+
   return (
-    <div className="chat-list-item">
+    <div className="chat-list-item" onClick={handleClick}>
       <div className="chat-list-item-left">
         <Avatar src={profilePicUrl} alt={`${title}'s photo`} size="56" />
       </div>
