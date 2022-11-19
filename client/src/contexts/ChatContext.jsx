8c276fc76c2d81a2d0f4d454af13cad13e40c0f1
Josel Catalan <catalanjosel123@gmail.com>
2022-11-19 15:00:14 +0800

    chore: implement dummy chat data in ChatContext

--- client/src/contexts/ChatContext.jsx
+++ client/src/contexts/ChatContext.jsx
@@ -1,12 +1,5 @@
 import { createContext, useState } from 'react'
-
-// TODO: This data should come from api
-const initialState = {
-  chatId: '1',
-  title: 'Chat 1',
-  profilePicUrl:
-    'https://images.pexels.com/photos/381228/pexels-photo-381228.jpeg?auto=compress&cs=tinysrgb&w=1600',
-}
+import initialState from '../data/chat.json'
 
 const ChatContext = createContext(initialState)
 
