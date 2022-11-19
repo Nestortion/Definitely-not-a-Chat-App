91702b36a11d3087eaef91cc954cb7736b9c5f3d
Josel Catalan <catalanjosel123@gmail.com>
2022-11-19 15:37:09 +0800

    chore: add memberList logic and some styling to RightBar

--- client/src/components/RightBar/RightBar.jsx
+++ client/src/components/RightBar/RightBar.jsx
@@ -2,20 +2,26 @@ import './rightbar.scss'
 import Avatar from '../UI/Avatar/Avatar'
 import { useContext } from 'react'
 import ChatContext from '../../contexts/ChatContext'
+import Button from '../UI/Button/Button'
 
 export default function RightBar() {
   const { chat } = useContext(ChatContext)
 
+  // TODO: Create a component maybe?
   // * Loop through members property keys
   // * then loop through its elements
-  const membersList = Object.keys(chat.members).map((key) => (
-    <ul>
-      <span>{key}</span>
-      {chat.members[key].map((element) => (
-        <li>{element}</li>
+  const membersList = (
+    <div>
+      {Object.entries(chat.members).map(([memberType, memberTypeMembers]) => (
+        <ul>
+          <span>{memberType}</span>
+          {memberTypeMembers.map((member) => (
+            <li>{member}</li>
+          ))}
+        </ul>
       ))}
-    </ul>
-  ))
+    </div>
+  )
 
   return (
     <div className="rightbar">
@@ -27,7 +33,14 @@ export default function RightBar() {
         />
         <span>{chat.title}</span>
       </div>
-      <div className="rightbar--main">{membersList}</div>
+      <div className="rightbar--main">
+        {membersList}
+        <div>
+          <Button>Add member</Button>
+          <Button>Search in Group</Button>
+          <Button>Report Chat</Button>
+        </div>
+      </div>
     </div>
   )
 }
