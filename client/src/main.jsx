a5c3d237356dc82cffec515e9467f60df3dfe150
Nestortion <gerona.nestor@gmail.com>
2022-11-26 19:00:32 +0800

    client: setup apollo client to connect to the server

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,9 +8,17 @@ import Home from './pages/Home/Home'
 import Chat from './pages/Chat/Chat'
 import Profile from './pages/Profile/Profile'
 import './globals.scss'
-import { AuthContextProvider } from './contexts/AuthContext'
-import { ChatContextProvider } from './contexts/ChatContext'
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute'
+import {
+  ApolloProvider,
+  InMemoryCache,
+  ApolloClient,
+  ApolloLink,
+} from '@apollo/client'
+import { createUploadLink } from 'apollo-upload-client'
+import jwt_decode from 'jwt-decode'
+import { TokenRefreshLink } from 'apollo-link-token-refresh'
+import { getAccessToken, setAccessToken } from './graphql/authStore.js'
 
 const router = createBrowserRouter([
   {
@@ -45,12 +53,66 @@ const router = createBrowserRouter([
   },
 ])
 
+const refreshTokenLink = new TokenRefreshLink({
+  accessTokenField: 'accessToken',
+  isTokenValidOrUndefined: () => {
+    const token = getAccessToken()
+    if (!token) {
+      return true
+    }
+    try {
+      const { exp } = jwt_decode(token)
+      if (Date.now() >= exp * 1000) {
+        return false
+      }
+    } catch {
+      return true
+    }
+  },
+  fetchAccessToken: () => {
+    return fetch('http://localhost:4000/refresh_token', {
+      method: 'POST',
+      credentials: 'include',
+    })
+  },
+  handleFetch: (accessToken) => {
+    setAccessToken(accessToken)
+  },
+
+  handleError: (err) => {
+    console.warn('token is invalid or expired')
+    console.log(err)
+  },
+})
+
+const authLink = new ApolloLink((operation, forward) => {
+  operation.setContext(({ headers = {} }) => {
+    const accessToken = getAccessToken()
+    return {
+      headers: {
+        ...headers,
+        authorization: `bearer ${accessToken}`,
+      },
+    }
+  })
+
+  return forward(operation)
+})
+
+const uploadlink = createUploadLink({
+  uri: 'http://localhost:4000/graphql',
+  credentials: 'include',
+})
+
+const client = new ApolloClient({
+  link: ApolloLink.from([refreshTokenLink, authLink, uploadlink]),
+  cache: new InMemoryCache(),
+})
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthContextProvider>
-      <ChatContextProvider>
-        <RouterProvider router={router} />
-      </ChatContextProvider>
-    </AuthContextProvider>
+    <ApolloProvider client={client}>
+      <RouterProvider router={router} />
+    </ApolloProvider>
   </React.StrictMode>
 )
