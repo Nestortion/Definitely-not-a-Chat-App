38d50d5c42872272eef55a0f5bc3902adac8b39c
Nestortion <gerona.nestor@gmail.com>
2022-11-26 22:51:50 +0800

    client: replaced userrContext with serverAuth

--- client/src/components/ProtectedRoute/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,11 +1,10 @@
-import { useContext } from 'react'
 import { Navigate } from 'react-router-dom'
-import AuthContext from '../../contexts/AuthContext'
+import { useIsLoggedInQuery } from '../../graphql/hooks/graphql'
 
 export default function ProtectedRoute({ children }) {
-  const { user } = useContext(AuthContext)
+  const { data: isLogged } = useIsLoggedInQuery()
 
-  if (!user) {
+  if (isLogged?.isLoggedIn === false) {
     return <Navigate to="/login" />
   }
 
