fcefe88816bf578a0fa2dbe090db6d8d550f4d9f
Josel Catalan <catalanjosel123@gmail.com>
2022-11-27 18:36:16 +0800

    chore: create dummy data on NavBar for dev purposes

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,12 +1,17 @@
 import './navbar.scss'
 import { Link } from 'react-router-dom'
-import { useContext } from 'react'
-import AuthContext from '../../contexts/AuthContext'
 import UserCard from '../UserCard/UserCard'
 
-export default function NavBar() {
-  const { user, setUser } = useContext(AuthContext)
+// ! TEMPORARY DATA ONLY
+// data should come from api
+const user = {
+  userId: '1',
+  username: 'John Doe',
+  profilePicUrl:
+    'https://images.pexels.com/photos/12050399/pexels-photo-12050399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+}
 
+export default function NavBar() {
   return (
     <div className="navbar">
       <div className="left">
