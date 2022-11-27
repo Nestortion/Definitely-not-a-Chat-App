d7ab68e40d74b0cf9c20fcce96f6fd3d17e1b21f
Nestortion <gerona.nestor@gmail.com>
2022-11-27 21:12:49 +0800

    client: will no refresh accesstoken as long as user is logged in

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,23 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
 import Layout from './components/Layout/Layout'
+import { setAccessToken } from './graphql/authStore'
 
 export default function App() {
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    fetch('http://localhost:4000/refresh_token', {
+      method: 'POST',
+      credentials: 'include',
+    }).then(async (res) => {
+      const accessToken = await res.json()
+      setAccessToken(accessToken)
+      setLoading(false)
+    })
+  }, [])
+
+  if (loading) return <h1>loading...</h1>
+
   return (
     <>
       <Layout />
