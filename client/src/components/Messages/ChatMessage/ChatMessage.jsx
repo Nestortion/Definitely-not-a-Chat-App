2f1ddd0299013e81be15262b860d8b92a6363a0d
Josel Catalan <catalanjosel123@gmail.com>
2022-11-27 19:28:19 +0800

    chore: add support for file and image chats in ChatMessage

--- client/src/components/Messages/ChatMessage/ChatMessage.jsx
+++ client/src/components/Messages/ChatMessage/ChatMessage.jsx
@@ -1,6 +1,25 @@
 import './chat-message.scss'
 
-export default function ChatMessage({ text, sender }) {
+export default function ChatMessage({
+  text,
+  sender,
+  isImage,
+  isFile,
+  url,
+  fileName,
+}) {
+  if (isImage) {
+    return <img src={url} />
+  }
+
+  if (isFile) {
+    return (
+      <a href={url} download>
+        {fileName}
+      </a>
+    )
+  }
+
   return (
     <div className={`chat-message ${sender}`}>
       <span>{text}</span>
