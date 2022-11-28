f647e0116399c79bcff9b075724d9a2b265cb37c
Josel Catalan <catalanjosel123@gmail.com>
2022-11-28 17:57:31 +0800

    chore: only show RightBar if the current page is Chat page

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,23 +1,41 @@
-import { Outlet } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
 import LeftBar from '../LeftBar/LeftBar'
 import NavBar from '../NavBar/NavBar'
 import RightBar from '../RightBar/RightBar'
 import './layout.scss'
 
 export default function Layout() {
+  const location = useLocation()
+
+  const isInChatPage = location.pathname.split('/')[1] === 'chat'
+  const [leftShouldShow, setLeftShouldShow] = useState(true)
+  const [middleShouldShow, setMiddleShouldShow] = useState(true)
+  const [rightShouldShow, setRightShouldShow] = useState(false)
+
+  useEffect(() => {
+    setRightShouldShow(isInChatPage)
+  }, [isInChatPage])
+
   return (
     <div className="layout">
       <NavBar />
       <div className="main">
-        <div className="left">
-          <LeftBar />
-        </div>
-        <div className="middle">
-          <Outlet />
-        </div>
-        <div className="right">
-          <RightBar />
-        </div>
+        {leftShouldShow && (
+          <div className="left">
+            <LeftBar />
+          </div>
+        )}
+        {middleShouldShow && (
+          <div className="middle">
+            <Outlet />
+          </div>
+        )}
+        {rightShouldShow && (
+          <div className="right">
+            <RightBar />
+          </div>
+        )}
       </div>
     </div>
   )
