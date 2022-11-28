bfd6e96d210161eef0f356a4594bbd823a3b8226
Josel Catalan <catalanjosel123@gmail.com>
2022-11-28 17:28:47 +0800

    chore: add logout support (but not implement) on UserCard component

--- client/src/components/UserCard/UserCard.jsx
+++ client/src/components/UserCard/UserCard.jsx
@@ -6,13 +6,17 @@ import { useState } from 'react'
 export default function UserCard({ profilePicUrl, username }) {
   const [userSettingsIsShowing, setUserSettingsIsShowing] = useState(true)
 
-  const handleClick = () => {
+  const toggleUserSettings = () => {
     setUserSettingsIsShowing((prev) => !prev)
+  }
+
+  const handleLogout = () => {
+    console.log('User logged out!')
   }
 
   return (
     <div className="user-card">
-      <div className="user-card__user-info" onClick={handleClick}>
+      <div className="user-card__user-info" onClick={toggleUserSettings}>
         <Avatar
           src={profilePicUrl}
           alt={`${username}'s profile pic`}
@@ -22,7 +26,7 @@ export default function UserCard({ profilePicUrl, username }) {
       </div>
       {userSettingsIsShowing && (
         <div className="user-card__user-settings">
-          <Button>Log out</Button>
+          <Button onClick={handleLogout}>Log out</Button>
         </div>
       )}
     </div>
