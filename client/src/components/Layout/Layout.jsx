fe5a77091336935325e0fcee8c0d0b7bdd26dd85
Josel Catalan <catalanjosel123@gmail.com>
2022-11-28 19:28:31 +0800

    feat: implement responsiveness for tablet and mobile devices

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -5,6 +5,7 @@ import NavBar from '../NavBar/NavBar'
 import RightBar from '../RightBar/RightBar'
 import './layout.scss'
 import { useMediaQuery } from 'react-responsive'
+import Button from '../UI/Button/Button'
 
 export default function Layout() {
   const location = useLocation()
@@ -15,6 +16,24 @@ export default function Layout() {
   const [leftShouldShow, setLeftShouldShow] = useState(true)
   const [middleShouldShow, setMiddleShouldShow] = useState(true)
   const [rightShouldShow, setRightShouldShow] = useState(false)
+
+  const showOnlyLeft = () => {
+    setLeftShouldShow(true)
+    setMiddleShouldShow(false)
+    setRightShouldShow(false)
+  }
+
+  const showOnlyMiddle = () => {
+    setLeftShouldShow(false)
+    setMiddleShouldShow(true)
+    setRightShouldShow(false)
+  }
+
+  const showOnlyRight = () => {
+    setLeftShouldShow(false)
+    setMiddleShouldShow(false)
+    setRightShouldShow(true)
+  }
 
   // On desktop only show rightbar when in chat page
   useEffect(() => {
@@ -28,27 +47,46 @@ export default function Layout() {
   // On tablet or mobile only show leftbar first
   useEffect(() => {
     if (isTabletOrMobile) {
-      setMiddleShouldShow(false)
-      setRightShouldShow(false)
+      showOnlyLeft()
     }
   }, [])
+
+  console.log(isTabletOrMobile)
 
   return (
     <div className="layout">
       <NavBar />
       <div className="main">
         {leftShouldShow && (
-          <div className="left">
+          <div
+            className="left"
+            onClick={() => {
+              if (isTabletOrMobile) showOnlyMiddle()
+            }}
+          >
             <LeftBar />
           </div>
         )}
+
         {middleShouldShow && (
           <div className="middle">
+            {isTabletOrMobile && (
+              <div className="middle__button-group">
+                <Button onClick={showOnlyLeft}>back</Button>
+                <Button onClick={showOnlyRight}>settings</Button>
+              </div>
+            )}
             <Outlet />
           </div>
         )}
+
         {rightShouldShow && (
           <div className="right">
+            {isTabletOrMobile && (
+              <div className="right__button-group">
+                <Button onClick={showOnlyMiddle}>back</Button>
+              </div>
+            )}
             <RightBar />
           </div>
         )}
