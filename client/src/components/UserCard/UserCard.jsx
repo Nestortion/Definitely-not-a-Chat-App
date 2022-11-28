8793a8c9cf29da85e9bb9d730ff62422e1415ea0
Josel Catalan <catalanjosel123@gmail.com>
2022-11-28 17:26:05 +0800

    chore: create a toggleable user settings on UserCard component

--- client/src/components/UserCard/UserCard.jsx
+++ client/src/components/UserCard/UserCard.jsx
@@ -1,11 +1,30 @@
 import Avatar from '../UI/Avatar/Avatar'
 import './user-card.scss'
+import Button from '../UI/Button/Button'
+import { useState } from 'react'
 
 export default function UserCard({ profilePicUrl, username }) {
+  const [userSettingsIsShowing, setUserSettingsIsShowing] = useState(true)
+
+  const handleClick = () => {
+    setUserSettingsIsShowing((prev) => !prev)
+  }
+
   return (
     <div className="user-card">
-      <Avatar src={profilePicUrl} alt={`${username}'s profile pic`} size="48" />
-      <span>{username}</span>
+      <div className="user-card__user-info" onClick={handleClick}>
+        <Avatar
+          src={profilePicUrl}
+          alt={`${username}'s profile pic`}
+          size="48"
+        />
+        <span>{username}</span>
+      </div>
+      {userSettingsIsShowing && (
+        <div className="user-card__user-settings">
+          <Button>Log out</Button>
+        </div>
+      )}
     </div>
   )
 }
