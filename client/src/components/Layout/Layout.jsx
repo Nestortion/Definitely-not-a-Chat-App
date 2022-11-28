e7afeecb67385ecbd5c7c6fdaf90fb33164e7025
Josel Catalan <catalanjosel123@gmail.com>
2022-11-28 18:33:55 +0800

    chore: install react-responsive package and make the site a little responsive

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -4,18 +4,34 @@ import LeftBar from '../LeftBar/LeftBar'
 import NavBar from '../NavBar/NavBar'
 import RightBar from '../RightBar/RightBar'
 import './layout.scss'
+import { useMediaQuery } from 'react-responsive'
 
 export default function Layout() {
   const location = useLocation()
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 961px)' })
 
   const isInChatPage = location.pathname.split('/')[1] === 'chat'
+
   const [leftShouldShow, setLeftShouldShow] = useState(true)
   const [middleShouldShow, setMiddleShouldShow] = useState(true)
   const [rightShouldShow, setRightShouldShow] = useState(false)
 
+  // On desktop only show rightbar when in chat page
   useEffect(() => {
-    setRightShouldShow(isInChatPage)
-  }, [isInChatPage])
+    if (!isTabletOrMobile && isInChatPage) {
+      setRightShouldShow(true)
+    } else {
+      setRightShouldShow(false)
+    }
+  }, [isInChatPage, isTabletOrMobile])
+
+  // On tablet or mobile only show leftbar first
+  useEffect(() => {
+    if (isTabletOrMobile) {
+      setMiddleShouldShow(false)
+      setRightShouldShow(false)
+    }
+  }, [])
 
   return (
     <div className="layout">
