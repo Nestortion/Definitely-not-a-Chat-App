6ea952f4aaaca43fd0f64895b5283c930bf93e05
Josel Catalan <catalanjosel123@gmail.com>
2022-11-28 19:55:23 +0800

    chore: add onClick listener on NavBar for layout responsiveness

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,7 +1,6 @@
 import './navbar.scss'
 import { Link } from 'react-router-dom'
 import UserCard from '../UserCard/UserCard'
-
 // ! TEMPORARY DATA ONLY
 // data should come from api
 // ! FETCH HERE
@@ -13,11 +12,11 @@ const user = {
     'https://images.pexels.com/photos/12050399/pexels-photo-12050399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 }
 
-export default function NavBar() {
+export default function NavBar({ onClick }) {
   return (
     <div className="navbar">
       <div className="left">
-        <Link to="/">
+        <Link to="/" onClick={onClick}>
           <span>DNCA</span>
         </Link>
       </div>
