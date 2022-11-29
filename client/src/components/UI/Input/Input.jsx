b043c1ad6bcb6633b3db9dad52863dfa5ece9631
Josel Catalan <catalanjosel123@gmail.com>
2022-11-29 19:26:08 +0800

    chore: add styling on Input UI component

--- client/src/components/UI/Input/Input.jsx
+++ client/src/components/UI/Input/Input.jsx
@@ -1,8 +1,10 @@
+import './input.scss'
+
 export default function Input({ label, id, type, ...rest }) {
   return (
-    <div>
+    <div className="input-container">
       {label && <label htmlFor={id}>{label}</label>}
-      <input id={id} type={type} {...rest} />
+      <input id={id} type={type} className="input-container__input" {...rest} />
     </div>
   )
 }
