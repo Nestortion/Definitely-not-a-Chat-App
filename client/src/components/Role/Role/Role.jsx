1d084df84406e07069b5a7bb460ef49cf45ed75a
Josel Catalan <catalanjosel123@gmail.com>
2022-11-30 17:07:40 +0800

    chore: add Role styles

--- client/src/components/Role/Role/Role.jsx
+++ client/src/components/Role/Role/Role.jsx
@@ -2,6 +2,7 @@ import { useUserRolesQuery } from '../../../graphql/hooks/graphql'
 import ErrorText from '../../Error/ErrorText'
 import LoadingText from '../../Loading/LoadingText'
 import RoleMember from '../RoleMembers/RoleMember'
+import './role.scss'
 
 export default function Role({ id, name }) {
   // fetch member here base on role
@@ -15,15 +16,17 @@ export default function Role({ id, name }) {
   if (error) return <ErrorText>Error</ErrorText>
 
   return (
-    <div>
-      <h1>{name}</h1>
-      {members.userRoles.map((member) => (
-        <RoleMember
-          key={member.id}
-          member={member.first_name}
-          pfp={member.profile_img}
-        />
-      ))}
+    <div className="role__container">
+      <h1 className="fs-500">{name}</h1>
+      <div className="role__role-members">
+        {members.userRoles.map((member) => (
+          <RoleMember
+            key={member.id}
+            member={member.first_name}
+            pfp={member.profile_img}
+          />
+        ))}
+      </div>
     </div>
   )
 }
