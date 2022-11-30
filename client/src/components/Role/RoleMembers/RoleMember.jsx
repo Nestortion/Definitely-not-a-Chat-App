e2382cb8e0f69ba51b684d47e1037b6c23d6289a
Josel Catalan <catalanjosel123@gmail.com>
2022-11-30 16:56:16 +0800

    chore: add RoleMember styles

--- client/src/components/Role/RoleMembers/RoleMember.jsx
+++ client/src/components/Role/RoleMembers/RoleMember.jsx
@@ -1,10 +1,12 @@
 import { apiBasePath } from '../../../data/config'
 import Avatar from '../../UI/Avatar/Avatar'
+import './role-member.scss'
 
 export default function RoleMember({ member }) {
   return (
-    <div>
+    <div className="role-member">
       <Avatar size={20} src={`${apiBasePath}/pfp/amogusz.jpg`} />
+      <span className="fs-400">{member}</span>
     </div>
   )
 }
