f665bdbadaf4a7fbea50f27885cedb98e7fd99a3
Josel Catalan <catalanjosel123@gmail.com>
2022-11-30 14:19:01 +0800

    chore: add sender image to ChatMessage

--- client/src/components/Messages/ChatMessage/ChatMessage.jsx
+++ client/src/components/Messages/ChatMessage/ChatMessage.jsx
@@ -4,20 +4,23 @@ import Avatar from '../../UI/Avatar/Avatar'
 
 export default function ChatMessage({ text, sender, user, message_type }) {
   return (
-    <div
-      className={`chat-message text-neutral-100 ${
-        sender === user ? 'you' : 'other'
-      }`}
-    >
-      {message_type === 'IMAGE' && (
-        <Avatar size={56} src={`${apiBasePath}/message/images/${text}`} />
-      )}
-      {message_type === 'OTHER' && (
-        <a href={`${apiBasePath}/message/documents/${text}`} download>
-          {text}
-        </a>
-      )}
-      {message_type === 'TEXT' && <span>{text}</span>}
+    <div className="chat-message text-neutral-100">
+      <div className="chat-message-sender">
+        {sender !== user && <Avatar size={30} src={``} />}
+      </div>
+      <div
+        className={`chat-message-message ${sender === user ? 'you' : 'other'}`}
+      >
+        {message_type === 'IMAGE' && (
+          <Avatar size={56} src={`${apiBasePath}/message/images/${text}`} />
+        )}
+        {message_type === 'OTHER' && (
+          <a href={`${apiBasePath}/message/documents/${text}`} download>
+            {text}
+          </a>
+        )}
+        {message_type === 'TEXT' && <span>{text}</span>}
+      </div>
     </div>
   )
 }
