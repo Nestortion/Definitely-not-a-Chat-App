bb2ade94b64352ed1400045784173f745235182f
Josel Catalan <catalanjosel123@gmail.com>
2022-11-30 14:04:21 +0800

    chore: change the temporary colors to use primary and secondary colors

--- client/src/components/Messages/ChatMessage/ChatMessage.jsx
+++ client/src/components/Messages/ChatMessage/ChatMessage.jsx
@@ -3,27 +3,21 @@ import { apiBasePath } from '../../../data/config'
 import Avatar from '../../UI/Avatar/Avatar'
 
 export default function ChatMessage({ text, sender, user, message_type }) {
-  if (message_type === 'IMAGE') {
-    return (
-      <div className={`chat-message ${sender === user ? 'you' : 'other'}`}>
+  return (
+    <div
+      className={`chat-message text-neutral-100 ${
+        sender === user ? 'you' : 'other'
+      }`}
+    >
+      {message_type === 'IMAGE' && (
         <Avatar size={56} src={`${apiBasePath}/message/images/${text}`} />
-      </div>
-    )
-  }
-
-  if (message_type === 'OTHER') {
-    return (
-      <div className={`chat-message ${sender === user ? 'you' : 'other'}`}>
+      )}
+      {message_type === 'OTHER' && (
         <a href={`${apiBasePath}/message/documents/${text}`} download>
           {text}
         </a>
-      </div>
-    )
-  }
-
-  return (
-    <div className={`chat-message ${sender === user ? 'you' : 'other'}`}>
-      <span>{text}</span>
+      )}
+      {message_type === 'TEXT' && <span>{text}</span>}
     </div>
   )
 }
