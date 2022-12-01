2c1142762f4750835c0e4c67e80591f084f9fcfa
Nestortion <gerona.nestor@gmail.com>
2022-12-01 17:37:49 +0800

    client: search is now functional

--- client/src/components/LeftBar/LeftBar.jsx
+++ client/src/components/LeftBar/LeftBar.jsx
@@ -3,15 +3,34 @@ import Button from '../UI/Button/Button'
 import Input from '../UI/Input/Input'
 import ChatList from '../ChatList/ChatList'
 import { useState } from 'react'
-import { useGroupsQuery } from '../../graphql/hooks/graphql'
+import {
+  useGroupsQuery,
+  useSearchGroupsLazyQuery,
+} from '../../graphql/hooks/graphql'
 import ErrorText from '../Error/ErrorText'
 import LoadingSpinner from '../Loading/LoadingSpinner/LoadingSpinner'
 import { MdAdd } from 'react-icons/md'
 
 export default function LeftBar({ showOnlyMiddle }) {
   const { data: chat, loading, error } = useGroupsQuery()
+  const [searchData, setSearchData] = useState()
+  const [searchGroups] = useSearchGroupsLazyQuery()
+  const [isSearching, setIsSearching] = useState(false)
+
   if (loading) return <LoadingSpinner></LoadingSpinner>
   if (error) return <ErrorText>Something went wrong</ErrorText>
+
+  const searchValueHandle = async (e) => {
+    if (e.target.value) {
+      const searchRes = await searchGroups({
+        variables: { groupName: e.target.value },
+      })
+      setSearchData(searchRes.data)
+      setIsSearching(true)
+    } else {
+      setIsSearching(false)
+    }
+  }
 
   return (
     <div className="leftbar">
@@ -24,13 +43,20 @@ export default function LeftBar({ showOnlyMiddle }) {
         </div>
         <div className="search">
           <form>
-            <Input type="text" placeholder="Search Chats" />
+            <Input
+              onChange={searchValueHandle}
+              type="text"
+              placeholder="Search Chats"
+            />
           </form>
         </div>
       </div>
 
       <div className="bottom">
-        <ChatList chats={chat.groups} showOnlyMiddle={showOnlyMiddle} />
+        <ChatList
+          chats={isSearching ? searchData.searchGroups : chat.groups}
+          showOnlyMiddle={showOnlyMiddle}
+        />
       </div>
     </div>
   )
