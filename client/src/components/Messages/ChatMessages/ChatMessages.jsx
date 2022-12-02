198cbfbd20e7224cd9f1ed38c583824032dfde72
Nestortion <gerona.nestor@gmail.com>
2022-12-02 18:51:07 +0800

    client: chat messages are no real-time

--- client/src/components/Messages/ChatMessages/ChatMessages.jsx
+++ client/src/components/Messages/ChatMessages/ChatMessages.jsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom'
 import {
-  useChatAddedSubscription,
+  ChatAddedDocument,
   useCurrentUserQuery,
   useGroupQuery,
   useUserChatsQuery,
@@ -9,6 +9,7 @@ import ChatMessage from '../ChatMessage/ChatMessage'
 import './chat-messages.scss'
 import LoadingSpinner from '../../Loading/LoadingSpinner/LoadingSpinner'
 import ErrorText from '../../Error/ErrorText'
+import { useEffect } from 'react'
 
 // ! FETCH HERE
 // fetch messages from a chat/group chat
@@ -22,11 +23,9 @@ export default function ChatMessages() {
   } = useCurrentUserQuery()
 
   const { chatId } = useParams()
-  const {
-    data: chats,
-    loading: chatsLoading,
-    error: chatsError,
-  } = useUserChatsQuery({ variables: { receiver: parseInt(chatId) } })
+  const chatsQuery = useUserChatsQuery({
+    variables: { receiver: parseInt(chatId) },
+  })
 
   const {
     data: groupData,
@@ -35,27 +34,34 @@ export default function ChatMessages() {
   } = useGroupQuery({
     variables: { groupId: parseInt(chatId) },
   })
-  const { data: subData, loading: subLoading } = useChatAddedSubscription({
-    onData: (data) => {
-      console.log('wow new data xdd')
-    },
-  })
-  if (!subLoading) {
-    console.log(subData)
-  }
+
+  useEffect(() => {
+    chatsQuery.subscribeToMore({
+      document: ChatAddedDocument,
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData.data) return prev
+        let previousChats = prev.userChats
+        return {
+          userChats: [...previousChats, subscriptionData.data.chatAdded],
+        }
+      },
+    })
+    return () => chatsQuery
+  }, [user])
+
   if (groupLoading) return <LoadingSpinner>Loading</LoadingSpinner>
   if (groupError) return <ErrorText>Error</ErrorText>
   if (userLoading) return <LoadingSpinner>Loading</LoadingSpinner>
   if (userError) return <ErrorText>Error</ErrorText>
-  if (chatsLoading) return <LoadingSpinner>Loading</LoadingSpinner>
-  if (chatsError) return <ErrorText>Error</ErrorText>
+  if (chatsQuery.loading) return <LoadingSpinner>Loading</LoadingSpinner>
+  if (chatsQuery.error) return <ErrorText>Error</ErrorText>
 
   return (
     <div className="chat-messages">
-      {chats.userChats.map((chatMessage) => (
+      {chatsQuery.data.userChats.map((chatMessage, index) => (
         <ChatMessage
           user={user.currentUser.id}
-          key={chatMessage.id}
+          key={index}
           text={chatMessage.message}
           sender={chatMessage.user_id}
           message_type={chatMessage.message_type}
