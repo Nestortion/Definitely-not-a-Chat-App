0b6d23e0d63f9934ab2046cf8499a99db84ff898
Josel Catalan <catalanjosel123@gmail.com>
2022-12-03 16:25:55 +0800

    chore: Modal styling

--- client/src/components/UI/Modal/SpawnModal.jsx
+++ client/src/components/UI/Modal/SpawnModal.jsx
@@ -1,5 +1,7 @@
 import ReactDOM from 'react-dom'
+import Card from '../Card/Card'
 import './modal.scss'
+import { MdClose } from 'react-icons/md'
 
 function Backdrop({ children, closeModal }) {
   return (
@@ -11,11 +13,15 @@ function Backdrop({ children, closeModal }) {
 
 function Modal({ title, children, closeModal }) {
   return (
-    <div className="modal">
-      <h1>{title}</h1>
-      {children}
-      <button onClick={closeModal}>Okay</button>
-    </div>
+    <Card onClick={(e) => e.stopPropagation()}>
+      <div className="modal">
+        <div className="modal__title">
+          <span>{title}</span>
+          <MdClose cursor="pointer" onClick={closeModal} />
+        </div>
+        <div className="modal__body">{children}</div>
+      </div>
+    </Card>
   )
 }
 
