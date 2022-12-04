ba8678620f333595bf4ca3f21dd6097b4f23b6b5
Josel Catalan <catalanjosel123@gmail.com>
2022-12-04 12:04:05 +0800

    chore: implement AddMembers component

--- client/src/components/SettingsButtons/SettingsButtons.jsx
+++ client/src/components/SettingsButtons/SettingsButtons.jsx
@@ -1,22 +1,70 @@
 import './settings-buttons.scss'
 import Button from '../UI/Button/Button'
 import { MdAdd, MdSearch, MdReport } from 'react-icons/md'
+import SpawnModal from '../UI/Modal/SpawnModal'
+import AddMembers from './AddMembers/AddMembers'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 export default function SettingsButtons() {
+  const [shouldShowModal, setShouldShowModal] = useState(false)
+  const [shouldShowAddMembers, setShouldShowAddMembers] = useState(false)
+  const [shouldShowSearchInGroup, setShouldShowSearchInGroup] = useState(false)
+  const [shouldShowReportChat, setShouldShowReportChat] = useState(false)
+  const [currentTitle, setCurrentTitle] = useState()
+
+  const handleShowModal = () => {
+    setShouldShowModal(true)
+  }
+
+  const handleHideModal = () => {
+    setShouldShowModal(false)
+    setShouldShowAddMembers(false)
+    setShouldShowSearchInGroup(false)
+    setShouldShowReportChat(false)
+  }
+
+  const showAddMembers = () => {
+    setShouldShowAddMembers(true)
+    setCurrentTitle('Add Members')
+    handleShowModal()
+  }
+
+  const showSearchInGroup = () => {
+    setShouldShowSearchInGroup(true)
+    setCurrentTitle('Search in Group')
+    handleShowModal()
+  }
+
+  const showReportChat = () => {
+    setShouldShowReportChat(true)
+    setCurrentTitle('Report Chat')
+    handleShowModal()
+  }
+
   return (
-    <div className="settings-buttons">
-      <Button>
-        <MdAdd style={{ display: 'inline-block' }} />
-        <span>Add Members</span>
-      </Button>
-      <Button>
-        <MdSearch style={{ display: 'inline-block' }} />
-        <span>Search in Group</span>
-      </Button>
-      <Button>
-        <MdReport style={{ display: 'inline-block' }} />
-        <span>Report Chat</span>
-      </Button>
-    </div>
+    <>
+      {shouldShowModal && (
+        <SpawnModal title={currentTitle} closeModal={handleHideModal}>
+          {shouldShowAddMembers && <AddMembers />}
+          {shouldShowSearchInGroup && <span>Search</span>}
+          {shouldShowReportChat && <span>Report</span>}
+        </SpawnModal>
+      )}
+      <div className="settings-buttons">
+        <Button onClick={showAddMembers}>
+          <MdAdd style={{ display: 'inline-block' }} />
+          <span>Add Members</span>
+        </Button>
+        <Button onClick={showSearchInGroup}>
+          <MdSearch style={{ display: 'inline-block' }} />
+          <span>Search in Group</span>
+        </Button>
+        <Button onClick={showReportChat}>
+          <MdReport style={{ display: 'inline-block' }} />
+          <span>Report Chat</span>
+        </Button>
+      </div>
+    </>
   )
 }
