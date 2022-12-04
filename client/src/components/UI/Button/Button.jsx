824f9bc47f45acc0f6de334b395a88aa1f4f9b00
Josel Catalan <catalanjosel123@gmail.com>
2022-12-04 13:19:11 +0800

    chore: support for rest props on Button component

--- client/src/components/UI/Button/Button.jsx
+++ client/src/components/UI/Button/Button.jsx
@@ -1,8 +1,12 @@
 import './button.scss'
 
-export default function Button({ children, onClick }) {
+export default function Button({ children, onClick, secondary, ...rest }) {
   return (
-    <button className="button" onClick={onClick}>
+    <button
+      className={`button ${secondary ? 'secondary-btn' : ''}`}
+      onClick={onClick}
+      {...rest}
+    >
       {children}
     </button>
   )
