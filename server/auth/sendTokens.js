cdaf866f84af9b7343b2c43c333b446cb5ccc4cd
Nestortion <gerona.nestor@gmail.com>
2022-12-05 15:02:23 +0800

    server: comment samesite and secure

--- server/auth/sendTokens.js
+++ server/auth/sendTokens.js
@@ -2,7 +2,7 @@ export const sendRefreshToken = (res, token) => {
   res.cookie('refresh-token', token, {
     expiresIn: 60 * 60 * 24 * 7,
     httpOnly: true,
-    sameSite: 'None',
-    secure: true,
+    // sameSite: 'None',
+    // secure: true,
   })
 }
