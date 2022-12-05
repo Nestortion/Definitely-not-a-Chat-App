078ffc2dbb2c445742d29fd935c2d47c2b7373ca
Josel Catalan <catalanjosel123@gmail.com>
2022-12-05 16:26:26 +0800

    chore: style Profile page

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,3 +1,34 @@
+import Avatar from '../../components/UI/Avatar/Avatar'
+import { apiBasePath } from '../../data/config'
+import './profile.scss'
+
 export default function Profile() {
-  return <div>Profile</div>
+  const currentProfile = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 21,
+    gender: 'Male',
+    section: 'BSIT 4-1',
+    username: 'johndoe',
+    profilePicUrl: `${apiBasePath}/message/images/amogusz.jpg`,
+  }
+
+  return (
+    <div className="profile">
+      <div className="profile__header">
+        <div className="profile__image">
+          <Avatar src={currentProfile.profilePicUrl} size={256} />
+        </div>
+        <h1 className="text-primary-400">
+          {currentProfile.firstName} {currentProfile.lastName}
+        </h1>
+        <span>{currentProfile.username}</span>
+      </div>
+      <div className="profile__info">
+        <span>Age: {currentProfile.age}</span>
+        <span>Gender: {currentProfile.gender}</span>
+        <span>Section: {currentProfile.section}</span>
+      </div>
+    </div>
+  )
 }
