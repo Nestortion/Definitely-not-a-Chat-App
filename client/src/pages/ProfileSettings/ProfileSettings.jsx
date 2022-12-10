7d048ea73beab674a76d6c80f8bdf48faf6c2db1
Nestortion <gerona.nestor@gmail.com>
2022-12-10 16:28:14 +0800

    client: console.log server data

--- client/src/pages/ProfileSettings/ProfileSettings.jsx
+++ client/src/pages/ProfileSettings/ProfileSettings.jsx
@@ -1,3 +1,13 @@
+import ErrorText from '../../components/Error/ErrorText'
+import LoadingSpinner from '../../components/Loading/LoadingSpinner/LoadingSpinner'
+import { useUserQuery } from '../../graphql/hooks/graphql'
+
 export default function ProfileSettings() {
+  const { data: userData, userLoading, error: userError } = useUserQuery()
+
+  if (userLoading) return <LoadingSpinner />
+  if (userError) return <ErrorText />
+
+  console.log(userData)
   return <div>ProfileSettings</div>
 }
