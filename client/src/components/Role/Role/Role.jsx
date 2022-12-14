cc9275e750b62fd4c217bc9f1b9387b241c884b0
Nestortion <gerona.nestor@gmail.com>
2022-12-14 21:06:16 +0800

    client: roles no sub to member added sub

--- client/src/components/Role/Role/Role.jsx
+++ client/src/components/Role/Role/Role.jsx
@@ -1,18 +1,41 @@
-import { useUserRolesQuery } from '../../../graphql/hooks/graphql'
+import { useEffect } from 'react'
+import {
+  MemberAddedDocument,
+  useUserRolesQuery,
+} from '../../../graphql/hooks/graphql'
 import ErrorText from '../../Error/ErrorText'
 import LoadingSpinner from '../../Loading/LoadingSpinner/LoadingSpinner'
 import RoleMember from '../RoleMembers/RoleMember'
 import './role.scss'
 
-export default function Role({ id, name, emoji, showOnlyMiddle }) {
+export default function Role({
+  id,
+  name,
+  emoji,
+  showOnlyMiddle,
+  rolesRefetch,
+  user,
+}) {
   // fetch member here base on role
   const {
     data: members,
     loading,
     error,
+    subscribeToMore,
   } = useUserRolesQuery({ variables: { groupRoleId: id } })
 
-  if (loading) return <LoadingSpinner>Loading</LoadingSpinner>
+  useEffect(() => {
+    subscribeToMore({
+      document: MemberAddedDocument,
+      variables: { user: user.id },
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData.data) return prev
+        rolesRefetch()
+      },
+    })
+  }, [])
+
+  if (loading) return <LoadingSpinner />
   if (error) return <ErrorText>Error</ErrorText>
 
   return (
