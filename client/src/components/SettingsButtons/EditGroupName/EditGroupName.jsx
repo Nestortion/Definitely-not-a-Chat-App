0c5c58680fbc9cd00557a2554d6470b53a5a9021
Josel Catalan <catalanjosel123@gmail.com>
2023-01-14 20:54:30 +0800

    chore: implement delete group and confirmation modal in group settings

--- client/src/components/SettingsButtons/EditGroupName/EditGroupName.jsx
+++ client/src/components/SettingsButtons/EditGroupName/EditGroupName.jsx
@@ -5,6 +5,8 @@ import { useParams } from 'react-router-dom'
 import { useUpdateGroupNameMutation } from '../../../graphql/hooks/graphql'
 import { useRef } from 'react'
 import { toast } from 'react-toastify'
+import SpawnModal from '../../UI/Modal/SpawnModal'
+import { MdWarning } from 'react-icons/md'
 
 export default function GroupSettings({ closeModal }) {
   const notify = () =>
@@ -19,6 +21,7 @@ export default function GroupSettings({ closeModal }) {
 
   const [newName, setNewName] = useState('')
   const [newImage, setNewImage] = useState(null)
+  const [modalShouldShow, setModalShouldShow] = useState(false)
   const imageInputRef = useRef()
   const { chatId } = useParams()
   const [updateGroupName] = useUpdateGroupNameMutation()
@@ -46,7 +49,6 @@ export default function GroupSettings({ closeModal }) {
     updateGroupName({
       variables: { groupName: newName, groupId: parseInt(chatId) },
     })
-
     closeModal()
     notify()
   }
@@ -57,36 +59,73 @@ export default function GroupSettings({ closeModal }) {
     imageInputRef.current.value = null
   }
 
-  return (
-    <div className="edit-group-name">
-      <form onSubmit={handleSave}>
-        <div className="edit-group-name__input">
-          <label htmlFor="group-photo">New Group Photo</label>
-          <input
-            type="file"
-            id="group-photo"
-            accept="image/*"
-            onChange={handleImageChange}
-            ref={imageInputRef}
-          />
-        </div>
-        <div className="edit-group-name__input">
-          <label htmlFor="group-name">New Group Name</label>
-          <input
-            type="text"
-            id="group-name"
-            value={newName}
-            onChange={handleChange}
-          />
-        </div>
+  const showConfirmModal = () => {
+    setModalShouldShow(true)
+  }
 
-        <div className="edit-group-name__button-group">
-          <Button onClick={handleSave}>Save</Button>
-          <Button type="button" secondary onClick={handleReset}>
-            Reset
-          </Button>
-        </div>
-      </form>
-    </div>
+  const closeConfirmModal = () => {
+    setModalShouldShow(false)
+  }
+
+  const handleDeleteGroup = () => {
+    console.log('Group deleted')
+  }
+
+  return (
+    <>
+      {modalShouldShow && (
+        <SpawnModal title="Confirm" closeModal={closeConfirmModal}>
+          <div className="confirm-modal">
+            <span className="fw-bold fs-500">Are you sure?</span>
+            <div className="confirm-modal__button-group">
+              <Button onClick={handleDeleteGroup}>Yes</Button>
+              <Button onClick={closeConfirmModal} secondary>
+                No
+              </Button>
+            </div>
+          </div>
+        </SpawnModal>
+      )}
+      <div className="edit-group-name">
+        <form onSubmit={handleSave}>
+          <div className="edit-group-name__input">
+            <label htmlFor="group-photo" className="fw-bold">
+              New Group Photo
+            </label>
+            <input
+              type="file"
+              id="group-photo"
+              accept="image/*"
+              onChange={handleImageChange}
+              ref={imageInputRef}
+            />
+          </div>
+          <div className="edit-group-name__input">
+            <label htmlFor="group-name" className="fw-bold">
+              New Group Name
+            </label>
+            <input
+              type="text"
+              id="group-name"
+              value={newName}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="edit-group-name__input">
+            <Button onClick={showConfirmModal} secondary>
+              <MdWarning style={{ display: 'inline-block' }} />
+              <span>Delete Group</span>
+            </Button>
+          </div>
+
+          <div className="edit-group-name__button-group">
+            <Button onClick={handleSave}>Save</Button>
+            <Button type="button" secondary onClick={handleReset}>
+              Reset
+            </Button>
+          </div>
+        </form>
+      </div>
+    </>
   )
 }
