8b48113a1910757a9b6d03214f88e01896775dc2
Nestortion <gerona.nestor@gmail.com>
2023-01-14 23:14:38 +0800

    client: included isGroup in chatlistitem props

--- client/src/components/ChatList/ChatList.jsx
+++ client/src/components/ChatList/ChatList.jsx
@@ -14,6 +14,7 @@ export default function ChatList({ chats, showOnlyMiddle, latest }) {
               (latestChat) => latestChat && latestChat.receiver === chat.id
             )
           }
+          isGroup={chat.is_group}
           profilePicUrl={chat.group_picture}
           title={chat.group_name}
           showOnlyMiddle={showOnlyMiddle}
