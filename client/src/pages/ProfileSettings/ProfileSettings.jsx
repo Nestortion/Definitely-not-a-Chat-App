30010d15ad830b0206924b8f879cccbcdf46ac60
Josel Catalan <catalanjosel123@gmail.com>
2023-01-15 15:55:25 +0800

    chore: add confirmation modal on profile save

--- client/src/pages/ProfileSettings/ProfileSettings.jsx
+++ client/src/pages/ProfileSettings/ProfileSettings.jsx
@@ -8,8 +8,11 @@ import { useState } from 'react'
 import { useNavigate, useOutletContext } from 'react-router-dom'
 import Button from '../../components/UI/Button/Button'
 import { useUpdateUserProfileMutation } from '../../graphql/hooks/graphql'
+import SpawnModal from '../../components/UI/Modal/SpawnModal'
 
 export default function ProfileSettings() {
+  const [modalShouldShow, setModalShouldShow] = useState(false)
+
   const user = useOutletContext()
   const navigate = useNavigate()
 
@@ -71,127 +74,153 @@ export default function ProfileSettings() {
       },
     })
 
-    // ! ADD CONFIRMATION MODAL
+    console.log('bruh')
+
     navigate(0)
   }
 
+  const showConfirmModal = () => {
+    setModalShouldShow(true)
+  }
+
+  const closeConfirmModal = () => {
+    setModalShouldShow(false)
+  }
+
   return (
-    <div className="profile-settings">
-      <form onSubmit={handleSubmit}>
-        <div className="profile-settings__header">
-          <div className="profile-settings__image">
-            <Avatar src={values.profileLink} size={256} />
-            <div className="profile-settings__file-input">
-              <label
-                className="profile-settings__file-input__label"
-                htmlFor="image"
-              >
-                <MdUpload />
+    <>
+      {modalShouldShow && (
+        <SpawnModal title="Confirm" closeModal={closeConfirmModal}>
+          <div className="confirm-modal">
+            <span className="fw-bold fs-500">Are you sure?</span>
+            <div className="confirm-modal__button-group">
+              <Button onClick={handleSubmit}>Yes</Button>
+              <Button onClick={closeConfirmModal} secondary>
+                No
+              </Button>
+            </div>
+          </div>
+        </SpawnModal>
+      )}
+      <div className="profile-settings">
+        <form onSubmit={handleSubmit}>
+          <div className="profile-settings__header">
+            <div className="profile-settings__image">
+              <Avatar src={values.profileLink} size={256} />
+              <div className="profile-settings__file-input">
+                <label
+                  className="profile-settings__file-input__label"
+                  htmlFor="image"
+                >
+                  <MdUpload />
+                </label>
+                <input
+                  className="profile-settings__file-input__input"
+                  type="file"
+                  accept="image/jpg, image/jpeg, image/png"
+                  id="image"
+                  onChange={handleImageChange}
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="profile-settings__info">
+            <div className="profile-settings__input-container">
+              <label htmlFor="firstName" className="text-primary-400">
+                First name:
               </label>
               <input
-                className="profile-settings__file-input__input"
-                type="file"
-                accept="image/jpg, image/jpeg, image/png"
-                id="image"
-                onChange={handleImageChange}
+                disabled
+                className="profile-settings-input"
+                type="text"
+                name="firstName"
+                onChange={handleChange}
+                id="firstName"
+                value={values.firstName}
+              />
+            </div>
+            <div className="profile-settings__input-container">
+              <label htmlFor="lastName" className="text-primary-400">
+                Last name:
+              </label>
+              <input
+                disabled
+                className="profile-settings-input"
+                type="text"
+                name="lastName"
+                onChange={handleChange}
+                id="lastName"
+                value={values.lastName}
+              />
+            </div>
+            <div className="profile-settings__input-container">
+              <label htmlFor="username">Username: </label>
+              <input
+                className="profile-settings-input"
+                type="text"
+                name="username"
+                onChange={handleChange}
+                id="username"
+                value={values.username}
+              />
+            </div>
+            <div className="profile-settings__input-container">
+              <label htmlFor="age">Age: </label>
+              <input
+                className="profile-settings-input"
+                type="number"
+                name="age"
+                onChange={handleChange}
+                id="age"
+                value={values.age}
+              />
+            </div>
+            <div className="profile-settings__input-container">
+              <label htmlFor="gender">Gender: </label>
+              <input
+                className="profile-settings-input"
+                type="text"
+                name="gender"
+                onChange={handleChange}
+                id="gender"
+                value={values.gender}
+              />
+            </div>
+            <div className="profile-settings__input-container">
+              <label htmlFor="section">Section: </label>
+              <input
+                className="profile-settings-input"
+                type="text"
+                name="section"
+                onChange={handleChange}
+                id="section"
+                value={values.section}
+              />
+            </div>
+            <div className="profile-settings__input-container">
+              <label htmlFor="address">Address: </label>
+              <input
+                className="profile-settings-input"
+                type="text"
+                name="address"
+                onChange={handleChange}
+                id="address"
+                value={values.address}
               />
             </div>
           </div>
-        </div>
 
-        <div className="profile-settings__info">
-          <div className="profile-settings__input-container">
-            <label htmlFor="firstName" className="text-primary-400">
-              First name:
-            </label>
-            <input
-              disabled
-              className="profile-settings-input"
-              type="text"
-              name="firstName"
-              onChange={handleChange}
-              id="firstName"
-              value={values.firstName}
-            />
+          <div className="profile-settings__button-group">
+            <Button type="button" onClick={showConfirmModal}>
+              Save
+            </Button>
+            <Button type="button" onClick={handleReset} secondary>
+              Reset
+            </Button>
           </div>
-          <div className="profile-settings__input-container">
-            <label htmlFor="lastName" className="text-primary-400">
-              Last name:
-            </label>
-            <input
-              disabled
-              className="profile-settings-input"
-              type="text"
-              name="lastName"
-              onChange={handleChange}
-              id="lastName"
-              value={values.lastName}
-            />
-          </div>
-          <div className="profile-settings__input-container">
-            <label htmlFor="username">Username: </label>
-            <input
-              className="profile-settings-input"
-              type="text"
-              name="username"
-              onChange={handleChange}
-              id="username"
-              value={values.username}
-            />
-          </div>
-          <div className="profile-settings__input-container">
-            <label htmlFor="age">Age: </label>
-            <input
-              className="profile-settings-input"
-              type="number"
-              name="age"
-              onChange={handleChange}
-              id="age"
-              value={values.age}
-            />
-          </div>
-          <div className="profile-settings__input-container">
-            <label htmlFor="gender">Gender: </label>
-            <input
-              className="profile-settings-input"
-              type="text"
-              name="gender"
-              onChange={handleChange}
-              id="gender"
-              value={values.gender}
-            />
-          </div>
-          <div className="profile-settings__input-container">
-            <label htmlFor="section">Section: </label>
-            <input
-              className="profile-settings-input"
-              type="text"
-              name="section"
-              onChange={handleChange}
-              id="section"
-              value={values.section}
-            />
-          </div>
-          <div className="profile-settings__input-container">
-            <label htmlFor="address">Address: </label>
-            <input
-              className="profile-settings-input"
-              type="text"
-              name="address"
-              onChange={handleChange}
-              id="address"
-              value={values.address}
-            />
-          </div>
-        </div>
-
-        <div className="profile-settings__button-group">
-          <Button>Save</Button>
-          <Button onClick={handleReset} secondary>
-            Reset
-          </Button>
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
+    </>
   )
 }
