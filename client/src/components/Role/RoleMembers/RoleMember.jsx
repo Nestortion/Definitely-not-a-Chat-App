c8318dff65557dec4c57ebb9b3ea02c7d31b6cf2
Josel Catalan <catalanjosel123@gmail.com>
2023-01-16 13:29:06 +0800

    chore: spawn modal on edit user role click

--- client/src/components/Role/RoleMembers/RoleMember.jsx
+++ client/src/components/Role/RoleMembers/RoleMember.jsx
@@ -10,8 +10,11 @@ import {
 } from '../../../graphql/hooks/graphql'
 import LoadingSpinner from '../../Loading/LoadingSpinner/LoadingSpinner'
 import ErrorText from '../../Error/ErrorText'
+import SpawnModal from '../../UI/Modal/SpawnModal'
+import { useState } from 'react'
 
 export default function RoleMember({ id, name, pfp, showOnlyMiddle, isGroup }) {
+  const [shouldShowModal, setShouldShowModal] = useState(false)
   const { chatId } = useParams()
   const navigate = useNavigate()
   const isTabletOrMobile = useMediaQuery({ query: '(max-width: 961px)' })
@@ -34,6 +37,14 @@ export default function RoleMember({ id, name, pfp, showOnlyMiddle, isGroup }) {
     navigate(`/profile/${id}`)
   }
 
+  const handleShowModal = () => {
+    setShouldShowModal(true)
+  }
+
+  const handleHideModal = () => {
+    setShouldShowModal(false)
+  }
+
   const handleUserSettings = () => {
     console.log(id)
   }
@@ -43,26 +54,33 @@ export default function RoleMember({ id, name, pfp, showOnlyMiddle, isGroup }) {
   }
 
   return (
-    <div className="role-member">
-      <div className="role-member__info">
-        <Avatar
-          size={20}
-          src={`${apiBasePath}/pfp/${pfp}`}
-          onClick={navigateToProfile}
-        />
-        <span className="fs-400">{name}</span>
+    <>
+      {shouldShowModal && (
+        <SpawnModal title="Edit user" closeModal={handleHideModal}>
+          <h1>Edit User Role</h1>
+        </SpawnModal>
+      )}
+      <div className="role-member">
+        <div className="role-member__info">
+          <Avatar
+            size={20}
+            src={`${apiBasePath}/pfp/${pfp}`}
+            onClick={navigateToProfile}
+          />
+          <span className="fs-400">{name}</span>
+        </div>
+        {isGroup === 'true' &&
+          userRoles.currentUserGroupRoles.roles.includes('MODERATOR') && (
+            <>
+              <button
+                className="fs-300 bg-secondary-400 text-neutral-100 role-member__button"
+                onClick={handleShowModal}
+              >
+                <MdSettings />
+              </button>
+            </>
+          )}
       </div>
-      {isGroup === 'true' &&
-        userRoles.currentUserGroupRoles.roles.includes('MODERATOR') && (
-          <>
-            <button
-              className="fs-300 bg-secondary-400 text-neutral-100 role-member__button"
-              onClick={handleUserSettings}
-            >
-              <MdSettings />
-            </button>
-          </>
-        )}
-    </div>
+    </>
   )
 }
