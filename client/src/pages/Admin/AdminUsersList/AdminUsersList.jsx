eb54742561628bc9ede9c07093ca1552f2956eb3
Nestortion <gerona.nestor@gmail.com>
2023-01-20 23:13:27 +0800

    client: admin User List page now dynamic

--- client/src/pages/Admin/AdminUsersList/AdminUsersList.jsx
+++ client/src/pages/Admin/AdminUsersList/AdminUsersList.jsx
@@ -1,33 +1,23 @@
 import { useState } from 'react'
 import { MdSettings } from 'react-icons/md'
 import Avatar from '../../../components/UI/Avatar/Avatar'
+import { useUsersQuery } from '../../../graphql/hooks/graphql'
 import './admin-users-list.scss'
+import LoadingSpinner from '../../../components/Loading/LoadingSpinner/LoadingSpinner'
+import ErrorText from '../../../components/Error/ErrorText'
+import { apiBasePath } from '../../../data/config'
 
 export default function AdminUsersList() {
   const [searchInput, setSearchInput] = useState('')
 
-  const [users, setUsers] = useState([
-    {
-      id: 1,
-      fullName: 'John Doe',
-      profilePicUrl: 'http://localhost:4000/pfp/johndoe.jpg',
-    },
-    {
-      id: 2,
-      fullName: 'Jane Doe',
-      profilePicUrl: 'http://localhost:4000/pfp/janedoe.jpg',
-    },
-    {
-      id: 3,
-      fullName: 'Josel Catalan',
-      profilePicUrl: 'http://localhost:4000/pfp/josel.jpg',
-    },
-    {
-      id: 4,
-      fullName: 'Nestor Gerona',
-      profilePicUrl: 'http://localhost:4000/pfp/nestor.jpg',
-    },
-  ])
+  const {
+    data: users,
+    loading: usersLoading,
+    error: usersError,
+  } = useUsersQuery()
+
+  if (usersLoading) return <LoadingSpinner />
+  if (usersError) return <ErrorText>Something Went Wrong</ErrorText>
 
   const handleChange = (e) => {
     setSearchInput(e.target.value)
@@ -47,14 +37,17 @@ export default function AdminUsersList() {
           />
         </div>
         <div className="admin-users-list__list-container">
-          {users.map((user) => (
+          {users.users.map((user) => (
             <div
               key={user.id}
               className="admin-users-list__list-user control-panel__card"
             >
-              <Avatar size={36} src={user.profilePicUrl} />
+              <Avatar
+                size={36}
+                src={`${apiBasePath}/pfp/${user.profile_img}`}
+              />
               <span>{user.id}</span>
-              <span>{user.fullName}</span>
+              <span>{`${user.first_name} ${user.last_name}`}</span>
               <span>
                 <MdSettings />
               </span>
