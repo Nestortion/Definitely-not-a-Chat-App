6b37290a5ec7bdceea8b7022a312428df63522d5
Nestortion <gerona.nestor@gmail.com>
2023-01-20 20:53:09 +0800

    client: added token persist logic

--- client/src/pages/Admin/Admin.jsx
+++ client/src/pages/Admin/Admin.jsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from 'react'
 import { Outlet } from 'react-router-dom'
+import LoadingSpinner from '../../components/Loading/LoadingSpinner/LoadingSpinner'
 import NavBar from '../../components/NavBar/NavBar'
+import { apiBasePath } from '../../data/config'
+import { setAccessToken } from '../../graphql/authStore'
 import './admin.scss'
 
 export default function Admin() {
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    fetch(`${apiBasePath}/refresh_token`, {
+      method: 'POST',
+      credentials: 'include',
+    }).then(async (res) => {
+      const accessToken = await res.json()
+      setAccessToken(accessToken.accessToken)
+      setLoading(false)
+    })
+  }, [])
+
+  if (loading) return <LoadingSpinner />
   return (
     <div className="admin-layout">
       <NavBar />
