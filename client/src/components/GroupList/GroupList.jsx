0deebd2d84e1eca204558bed275aa12d1ef92ec8
Nestortion <gerona.nestor@gmail.com>
2023-01-20 23:05:58 +0800

    client: GroupList now dynamic

--- client/src/components/GroupList/GroupList.jsx
+++ client/src/components/GroupList/GroupList.jsx
@@ -1,33 +1,23 @@
 import { useState } from 'react'
 import { MdSettings } from 'react-icons/md'
+import { apiBasePath } from '../../data/config'
+import { useGroupMembersQuery } from '../../graphql/hooks/graphql'
+import ErrorText from '../Error/ErrorText'
+import LoadingSpinner from '../Loading/LoadingSpinner/LoadingSpinner'
 import Avatar from '../UI/Avatar/Avatar'
 import './group-list.scss'
 
-export default function GroupList({ id, groupName, profilePicUrl }) {
-  const groupMembers = [
-    {
-      id: 1,
-      fullName: 'John Doe',
-      profilePicUrl: 'http://localhost:4000/pfp/johndoe.jpg',
-    },
-    {
-      id: 2,
-      fullName: 'Jane Doe',
-      profilePicUrl: 'http://localhost:4000/pfp/janedoe.jpg',
-    },
-    {
-      id: 3,
-      fullName: 'Josel Catalan',
-      profilePicUrl: 'http://localhost:4000/pfp/josel.jpg',
-    },
-    {
-      id: 4,
-      fullName: 'Nestor Gerona',
-      profilePicUrl: 'http://localhost:4000/pfp/nestor.jpg',
-    },
-  ]
+export default function GroupList({ id, groupName, profilePicUrl, isGroup }) {
+  const {
+    data: groupMembers,
+    loading: groupMembersLoading,
+    error: groupMembersError,
+  } = useGroupMembersQuery({ variables: { groupId: id } })
 
   const [shouldShowMembers, setShouldShowMembers] = useState(false)
+
+  if (groupMembersLoading) return <LoadingSpinner />
+  if (groupMembersError) return <ErrorText>Something Went Wrong</ErrorText>
 
   const toggleShowMembers = () => {
     setShouldShowMembers((prev) => !prev)
@@ -39,23 +29,31 @@ export default function GroupList({ id, groupName, profilePicUrl }) {
         className="group-list control-panel__card"
         onClick={toggleShowMembers}
       >
-        <Avatar size={36} src={profilePicUrl} />
+        <Avatar size={36} src={`${apiBasePath}/grouppfp/${profilePicUrl}`} />
         <span>{id}</span>
         <span>{groupName}</span>
-        <span>grouptype</span>
+        {isGroup === 'true' ? (
+          <span>Group Chat</span>
+        ) : (
+          <span>Private Chat</span>
+        )}
+
         <span>
           <MdSettings />
         </span>
       </div>
       {shouldShowMembers && (
         <div className="group-list__member-list control-panel__card">
-          {groupMembers.map((member) => (
+          {groupMembers.groupMembers.map((member) => (
             <div
               key={member.id}
               className="group-list__member control-panel__card"
             >
-              <Avatar size={24} src={member.profilePicUrl} />
-              <span>{member.fullName}</span>
+              <Avatar
+                size={24}
+                src={`${apiBasePath}/pfp/${member.profile_img}`}
+              />
+              <span>{`${member.first_name} ${member.last_name}`}</span>
             </div>
           ))}
         </div>
