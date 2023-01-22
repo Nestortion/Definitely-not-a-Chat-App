507dcbb00f49f62077de406fed9dcd91910dc974
Josel Catalan <catalanjosel123@gmail.com>
2023-01-22 20:31:49 +0800

    chore: implement accordion on user and admin logs

--- client/src/components/LogEntry/LogEntry.jsx
+++ client/src/components/LogEntry/LogEntry.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './log-entry.scss'
 
 export default function LogEntry({
@@ -7,27 +8,40 @@ export default function LogEntry({
   userId,
   section,
 }) {
+  const [detailShouldShow, setDetailShouldShow] = useState(false)
+
+  const toggleDetails = () => {
+    setDetailShouldShow((prev) => !prev)
+  }
+
   return (
     <div className="log-entry fs-400">
       <div className="log-entry__detail">
         <p className="fw-bold">{createdAt}:</p>
         <p>
-          <span className="text-secondary-400">{fullName}</span>{' '}
+          <span
+            className="text-secondary-400 log-entry__name"
+            onClick={toggleDetails}
+          >
+            {fullName}
+          </span>{' '}
           <span className="text-primary-400">{actionDescription}</span>
         </p>
       </div>
 
-      <div className="log-entry__user-detail">
-        <p>User Details:</p>
-        <p>
-          <span>Id: {userId}</span>
-        </p>
-        {section && (
+      {detailShouldShow && (
+        <div className="log-entry__user-detail">
+          <p>User Details:</p>
           <p>
-            <span>Section: {section}</span>
+            <span>Id: {userId}</span>
           </p>
-        )}
-      </div>
+          {section && (
+            <p>
+              <span>Section: {section}</span>
+            </p>
+          )}
+        </div>
+      )}
     </div>
   )
 }
