777a14330d9a1d1d111556f04b37d95e67589ab2
Josel Catalan <catalanjosel123@gmail.com>
2023-01-23 16:23:36 +0800

    chore: make gap larger on Log entries

--- client/src/components/LogEntry/LogEntry.jsx
+++ client/src/components/LogEntry/LogEntry.jsx
@@ -15,18 +15,16 @@ export default function LogEntry({
   }
 
   return (
-    <div className="log-entry fs-400">
+    <div className="log-entry">
+      <p className="fw-bold">{createdAt}:</p>
       <div className="log-entry__detail">
-        <p className="fw-bold">{createdAt}:</p>
-        <p>
-          <span
-            className="text-secondary-400 log-entry__name"
-            onClick={toggleDetails}
-          >
-            {fullName}
-          </span>{' '}
-          <span className="text-primary-400">{actionDescription}</span>
-        </p>
+        <span
+          className="text-secondary-400 log-entry__name"
+          onClick={toggleDetails}
+        >
+          {fullName}
+        </span>
+        <span className="text-primary-400">{actionDescription}</span>
       </div>
 
       {detailShouldShow && (
