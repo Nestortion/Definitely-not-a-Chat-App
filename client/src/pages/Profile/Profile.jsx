e919f89f8d7e561f1a227971dc169d9c64551527
Josel Catalan <catalanjosel123@gmail.com>
2023-01-24 18:11:45 +0800

    chore: add disable user button for admin users on Profile page

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -2,11 +2,21 @@ import { useParams } from 'react-router-dom'
 import ErrorText from '../../components/Error/ErrorText'
 import LoadingSpinner from '../../components/Loading/LoadingSpinner/LoadingSpinner'
 import Avatar from '../../components/UI/Avatar/Avatar'
+import Button from '../../components/UI/Button/Button'
 import { apiBasePath } from '../../data/config'
-import { useUserProfileQuery } from '../../graphql/hooks/graphql'
+import {
+  useCurrentUserQuery,
+  useUserProfileQuery,
+} from '../../graphql/hooks/graphql'
 import './profile.scss'
+import { MdWarning } from 'react-icons/md'
 
 export default function Profile() {
+  const {
+    data: currentUser,
+    loading: currentUserLoading,
+    error: currentUserError,
+  } = useCurrentUserQuery()
   const { profileId } = useParams()
   const {
     data: profileData,
@@ -14,6 +24,8 @@ export default function Profile() {
     error: profileError,
   } = useUserProfileQuery({ variables: { userProfileId: parseInt(profileId) } })
 
+  if (currentUserLoading) return <LoadingSpinner />
+  if (currentUserError) return <ErrorText />
   if (profileLoading) return <LoadingSpinner />
   if (profileError) return <ErrorText />
 
@@ -45,6 +57,14 @@ export default function Profile() {
         <span>Section: {profileData.userProfile.section}</span>
         <span>Address: {profileData.userProfile.address}</span>
       </div>
+      {currentUser.currentUser.access_level === 'ADMIN' &&
+        currentUser.currentUser.id !== +profileId && (
+          <div className="profile__button-group">
+            <Button secondary>
+              <MdWarning /> Disable User
+            </Button>
+          </div>
+        )}
     </div>
   )
 }
