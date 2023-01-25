2d7d3e6a37e6fa13959423cfd9039191c15f79e4
Nestortion <gerona.nestor@gmail.com>
2023-01-25 14:25:30 +0800

    client: made adminUserList search functional

--- client/src/pages/Admin/AdminUsersList/AdminUsersList.jsx
+++ client/src/pages/Admin/AdminUsersList/AdminUsersList.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { MdArrowForwardIos, MdSettings } from 'react-icons/md'
 import Avatar from '../../../components/UI/Avatar/Avatar'
 import { useUsersQuery } from '../../../graphql/hooks/graphql'
@@ -7,15 +7,36 @@ import LoadingSpinner from '../../../components/Loading/LoadingSpinner/LoadingSp
 import ErrorText from '../../../components/Error/ErrorText'
 import { apiBasePath } from '../../../data/config'
 import { Link } from 'react-router-dom'
+import useDebounceValue from '../../../helper_hooks/useDebounceValue'
 
 export default function AdminUsersList() {
   const [searchInput, setSearchInput] = useState('')
+  const [searchData, setSearchData] = useState([])
+  const [isSearching, setIsSearching] = useState(false)
+  const debounceValue = useDebounceValue(searchInput, 250)
 
   const {
     data: users,
     loading: usersLoading,
     error: usersError,
   } = useUsersQuery()
+
+  useEffect(() => {
+    ;(() => {
+      if (searchInput && searchInput.length > 0) {
+        const filteredData = users.users.filter((user) => {
+          const userFullName = `${user.first_name} ${user.last_name}`
+
+          return userFullName.toLowerCase().includes(debounceValue)
+        })
+
+        setSearchData(filteredData)
+        setIsSearching(true)
+      } else {
+        setIsSearching(false)
+      }
+    })()
+  }, [debounceValue])
 
   if (usersLoading) return <LoadingSpinner />
   if (usersError) return <ErrorText>Something Went Wrong</ErrorText>
@@ -38,32 +59,59 @@ export default function AdminUsersList() {
           />
         </div>
         <div className="admin-users-list__list-container">
-          {users.users.map((user) => (
-            <div
-              key={user.id}
-              className="admin-users-list__list-user control-panel__card"
-            >
-              <Avatar
-                size={36}
-                src={`${apiBasePath}/pfp/${user.profile_img}`}
-              />
-              <span>{user.id}</span>
-              <span>{`${user.first_name} ${user.last_name}`}</span>
-              <span>
-                <Link
-                  to={`/profile/${user.id}`}
-                  style={{
-                    textDecoration: 'none',
-                    color: 'var(--clr-neutral-900)',
-                    display: 'block',
-                    width: '100%',
-                  }}
+          {isSearching
+            ? searchData.map((user) => (
+                <div
+                  key={user.id}
+                  className="admin-users-list__list-user control-panel__card"
                 >
-                  <MdArrowForwardIos title="Go to profile page" />
-                </Link>
-              </span>
-            </div>
-          ))}
+                  <Avatar
+                    size={36}
+                    src={`${apiBasePath}/pfp/${user.profile_img}`}
+                  />
+                  <span>{user.id}</span>
+                  <span>{`${user.first_name} ${user.last_name}`}</span>
+                  <span>
+                    <Link
+                      to={`/profile/${user.id}`}
+                      style={{
+                        textDecoration: 'none',
+                        color: 'var(--clr-neutral-900)',
+                        display: 'block',
+                        width: '100%',
+                      }}
+                    >
+                      <MdArrowForwardIos title="Go to profile page" />
+                    </Link>
+                  </span>
+                </div>
+              ))
+            : users.users.map((user) => (
+                <div
+                  key={user.id}
+                  className="admin-users-list__list-user control-panel__card"
+                >
+                  <Avatar
+                    size={36}
+                    src={`${apiBasePath}/pfp/${user.profile_img}`}
+                  />
+                  <span>{user.id}</span>
+                  <span>{`${user.first_name} ${user.last_name}`}</span>
+                  <span>
+                    <Link
+                      to={`/profile/${user.id}`}
+                      style={{
+                        textDecoration: 'none',
+                        color: 'var(--clr-neutral-900)',
+                        display: 'block',
+                        width: '100%',
+                      }}
+                    >
+                      <MdArrowForwardIos title="Go to profile page" />
+                    </Link>
+                  </span>
+                </div>
+              ))}
         </div>
       </div>
     </div>
