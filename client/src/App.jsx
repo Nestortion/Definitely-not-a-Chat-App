95552b3d452e589dd79a5b942869d0eccb942436
Nestortion <gerona.nestor@gmail.com>
2023-01-25 13:57:31 +0800

    client: flush user sessions when user is disabled

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,23 @@ import { setAccessToken } from './graphql/authStore'
 import { apiBasePath } from './data/config'
 import LoadingSpinner from './components/Loading/LoadingSpinner/LoadingSpinner'
 import { atom, Provider } from 'jotai'
+import { useNavigate } from 'react-router-dom'
 
 export const searchInput = atom('')
 export const isSearching = atom(false)
 
 export default function App() {
   const [loading, setLoading] = useState(true)
+  const navigate = useNavigate()
   useEffect(() => {
     fetch(`${apiBasePath}/refresh_token`, {
       method: 'POST',
       credentials: 'include',
     }).then(async (res) => {
       const accessToken = await res.json()
+      if (accessToken.message) {
+        navigate('/login')
+      }
       setAccessToken(accessToken.accessToken)
       setLoading(false)
     })
