0fbab934c06c0677a7eb7496275cb70ca8bd4dee
Josel Catalan <catalanjosel123@gmail.com>
2023-01-25 18:25:41 +0800

    chore: add confirmation modal on admin edit profile

--- client/src/pages/Profile/AdminEditProfile/AdminEditProfile.jsx
+++ client/src/pages/Profile/AdminEditProfile/AdminEditProfile.jsx
@@ -4,6 +4,7 @@ import Avatar from '../../../components/UI/Avatar/Avatar'
 import Button from '../../../components/UI/Button/Button'
 import './admin-profile-settings.scss'
 import { toast } from 'react-toastify'
+import SpawnModal from '../../../components/UI/Modal/SpawnModal'
 
 export default function AdminEditProfile({ closeModal, profileId }) {
   const notify = (text) =>
@@ -31,6 +32,7 @@ export default function AdminEditProfile({ closeModal, profileId }) {
   }
 
   const [values, setValues] = useState(initialState)
+  const [confirmModalShouldShow, setConfirmModalShouldShow] = useState(false)
 
   const handleChange = (e) => {
     //? the age automatically converts to string
@@ -70,86 +72,109 @@ export default function AdminEditProfile({ closeModal, profileId }) {
     setValues(initialState)
   }
 
+  const showConfirmModal = () => {
+    setConfirmModalShouldShow(true)
+  }
+
+  const hideConfirmModal = () => {
+    setConfirmModalShouldShow(false)
+  }
+
   return (
-    <div className="admin-profile">
-      <form>
-        <div className="profile-settings__header">
-          <div className="profile-settings__image">
-            <Avatar src={values.profileLink} size={256} />
-            <div className="profile-settings__file-input">
-              <label
-                className="profile-settings__file-input__label"
-                htmlFor="image"
-              >
-                <MdUpload />
-              </label>
-              <input
-                className="profile-settings__file-input__input"
-                type="file"
-                accept="image/jpg, image/jpeg, image/png"
-                id="image"
-                onChange={handleImageChange}
-              />
+    <>
+      {confirmModalShouldShow && (
+        <SpawnModal title="Confirm" closeModal={hideConfirmModal}>
+          <div className="confirm-modal">
+            <p className="fw-bold">Are you sure?</p>
+            <div className="confirm-modal__button-group">
+              <Button onClick={handleSave}>Yes</Button>
+              <Button onClick={hideConfirmModal} secondary>
+                No
+              </Button>
             </div>
           </div>
-        </div>
-
-        <div className="profile-settings__info">
-          <div className="profile-settings__input-container">
-            <label htmlFor="firstName">First name:</label>
-            <input
-              className="profile-settings-input"
-              type="text"
-              name="firstName"
-              onChange={handleChange}
-              id="firstName"
-              value={values.firstName}
-            />
-          </div>
-          <div className="profile-settings__input-container">
-            <label htmlFor="lastName">Last name:</label>
-            <input
-              className="profile-settings-input"
-              type="text"
-              name="lastName"
-              onChange={handleChange}
-              id="lastName"
-              value={values.lastName}
-            />
-          </div>
-          <div className="profile-settings__input-container">
-            <label htmlFor="username">Username: </label>
-            <input
-              className="profile-settings-input"
-              type="text"
-              name="username"
-              onChange={handleChange}
-              id="username"
-              value={values.username}
-            />
-          </div>
-          <div className="profile-settings__input-container">
-            <label htmlFor="password">Change password: </label>
-            <input
-              className="profile-settings-input"
-              type="password"
-              name="password"
-              onChange={handleChange}
-              id="password"
-              value={values.password}
-            />
+        </SpawnModal>
+      )}
+      <div className="admin-profile">
+        <form>
+          <div className="profile-settings__header">
+            <div className="profile-settings__image">
+              <Avatar src={values.profileLink} size={256} />
+              <div className="profile-settings__file-input">
+                <label
+                  className="profile-settings__file-input__label"
+                  htmlFor="image"
+                >
+                  <MdUpload />
+                </label>
+                <input
+                  className="profile-settings__file-input__input"
+                  type="file"
+                  accept="image/jpg, image/jpeg, image/png"
+                  id="image"
+                  onChange={handleImageChange}
+                />
+              </div>
+            </div>
           </div>
 
-          <div className="profile-settings__button-group">
-            <Button type="button" onClick={handleSave}>
-              Save
-            </Button>
-            <Button type="button" secondary onClick={handleReset}>
-              Reset
-            </Button>
+          <div className="profile-settings__info">
+            <div className="profile-settings__input-container">
+              <label htmlFor="firstName">First name:</label>
+              <input
+                className="profile-settings-input"
+                type="text"
+                name="firstName"
+                onChange={handleChange}
+                id="firstName"
+                value={values.firstName}
+              />
+            </div>
+            <div className="profile-settings__input-container">
+              <label htmlFor="lastName">Last name:</label>
+              <input
+                className="profile-settings-input"
+                type="text"
+                name="lastName"
+                onChange={handleChange}
+                id="lastName"
+                value={values.lastName}
+              />
+            </div>
+            <div className="profile-settings__input-container">
+              <label htmlFor="username">Username: </label>
+              <input
+                className="profile-settings-input"
+                type="text"
+                name="username"
+                onChange={handleChange}
+                id="username"
+                value={values.username}
+              />
+            </div>
+            <div className="profile-settings__input-container">
+              <label htmlFor="password">Change password: </label>
+              <input
+                className="profile-settings-input"
+                type="password"
+                name="password"
+                onChange={handleChange}
+                id="password"
+                value={values.password}
+              />
+            </div>
+
+            <div className="profile-settings__button-group">
+              <Button type="button" onClick={showConfirmModal}>
+                Save
+              </Button>
+              <Button type="button" secondary onClick={handleReset}>
+                Reset
+              </Button>
+            </div>
           </div>
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
+    </>
   )
 }
