d0299f351495a9504e2ff6160dc427117bfa9f97
Nestortion <gerona.nestor@gmail.com>
2023-01-26 11:28:34 +0800

    server: updated Users model

--- server/models/Users.js
+++ server/models/Users.js
@@ -12,10 +12,9 @@ const Users = DB.define(
     },
     first_name: { type: DataTypes.STRING, allowNull: false },
     last_name: { type: DataTypes.STRING, allowNull: false },
-    age: { type: DataTypes.INTEGER, allowNull: false },
+    birthdate: { type: DataTypes.DATE, allowNull: false },
     address: { type: DataTypes.STRING, allowNull: false },
     gender: { type: DataTypes.STRING, allowNull: false },
-    section: { type: DataTypes.STRING, allowNull: false },
     username: { type: DataTypes.STRING, allowNull: false, unique: true },
     password: { type: DataTypes.STRING, allowNull: false },
     access_level: {
