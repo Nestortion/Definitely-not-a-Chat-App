c8f5bf8a3f47822c06be7329a24a4867252a83e9
Josel Catalan <catalanjosel123@gmail.com>
2023-01-29 20:38:38 +0800

    chore: remove delete group ui on group settings button

--- client/src/components/SettingsButtons/EditGroupName/EditGroupName.jsx
+++ client/src/components/SettingsButtons/EditGroupName/EditGroupName.jsx
@@ -5,8 +5,6 @@ import { useParams } from 'react-router-dom'
 import { useUpdateGroupMutation } from '../../../graphql/hooks/graphql'
 import { useRef } from 'react'
 import { toast } from 'react-toastify'
-import SpawnModal from '../../UI/Modal/SpawnModal'
-import { MdWarning } from 'react-icons/md'
 
 export default function GroupSettings({ closeModal }) {
   const notify = () =>
@@ -84,73 +82,39 @@ export default function GroupSettings({ closeModal }) {
     imageInputRef.current.value = null
   }
 
-  const showConfirmModal = () => {
-    setModalShouldShow(true)
-  }
-
-  const closeConfirmModal = () => {
-    setModalShouldShow(false)
-  }
-
-  const handleDeleteGroup = () => {
-    console.log('Group deleted')
-  }
-
   return (
-    <>
-      {modalShouldShow && (
-        <SpawnModal title="Confirm" closeModal={closeConfirmModal}>
-          <div className="confirm-modal">
-            <span className="fw-bold fs-500">Are you sure?</span>
-            <div className="confirm-modal__button-group">
-              <Button onClick={handleDeleteGroup}>Yes</Button>
-              <Button onClick={closeConfirmModal} secondary>
-                No
-              </Button>
-            </div>
-          </div>
-        </SpawnModal>
-      )}
-      <div className="edit-group-name">
-        <form onSubmit={handleSave}>
-          <div className="edit-group-name__input">
-            <label htmlFor="group-photo" className="fw-bold">
-              New Group Photo
-            </label>
-            <input
-              type="file"
-              id="group-photo"
-              accept="image/*"
-              onChange={handleImageChange}
-              ref={imageInputRef}
-            />
-          </div>
-          <div className="edit-group-name__input">
-            <label htmlFor="group-name" className="fw-bold">
-              New Group Name
-            </label>
-            <input
-              type="text"
-              id="group-name"
-              value={newName}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="edit-group-name__input">
-            <Button onClick={showConfirmModal} secondary>
-              <MdWarning style={{ display: 'inline-block' }} />
-              <span>Delete Group</span>
-            </Button>
-          </div>
-
-          <div className="edit-group-name__button-group">
-            <Button onClick={handleSave}>Save</Button>
-            <Button type="button" secondary onClick={handleReset}>
-              Reset
-            </Button>
-          </div>
-        </form>
-      </div>
-    </>
+    <div className="edit-group-name">
+      <form onSubmit={handleSave}>
+        <div className="edit-group-name__input">
+          <label htmlFor="group-photo" className="fw-bold">
+            New Group Photo
+          </label>
+          <input
+            type="file"
+            id="group-photo"
+            accept="image/*"
+            onChange={handleImageChange}
+            ref={imageInputRef}
+          />
+        </div>
+        <div className="edit-group-name__input">
+          <label htmlFor="group-name" className="fw-bold">
+            New Group Name
+          </label>
+          <input
+            type="text"
+            id="group-name"
+            value={newName}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="edit-group-name__button-group">
+          <Button onClick={handleSave}>Save</Button>
+          <Button type="button" secondary onClick={handleReset}>
+            Reset
+          </Button>
+        </div>
+      </form>
+    </div>
   )
 }
