0ba441cfcd92a86e414d2e4e60473920bf63146f
Josel Catalan <catalanjosel123@gmail.com>
2023-01-30 16:42:56 +0800

    chore: add some env vars on server

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,17 @@
 import { Sequelize } from 'sequelize'
 import dotenv from 'dotenv'
 dotenv.config()
-const DB = new Sequelize('dnca', 'root', `${process.env.DB_PASS}`, {
-  host: process.env.DB_URI || 'localhost',
-  port: process.env.DB_PORT || 3306,
-  dialect: 'mysql',
-  logging: false,
-})
+const DB = new Sequelize(
+  `${process.env.DB_NAME}`,
+  `${process.env.DB_USERNAME}`,
+  `${process.env.DB_PASS}`,
+  {
+    host: process.env.DB_URI || 'localhost',
+    port: process.env.DB_PORT || 3306,
+    dialect: 'mysql',
+    logging: false,
+  }
+)
 
 try {
   await DB.authenticate()
