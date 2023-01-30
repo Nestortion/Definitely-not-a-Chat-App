b3bec9fd016064cd40f21a4716c714f2cdb8a9d3
Nestortion <gerona.nestor@gmail.com>
2023-01-30 22:27:37 +0800

    server: removed sendtokens

--- server/auth/sendTokens.js
+++
@@ -1,8 +0,0 @@
-export const sendRefreshToken = (res, token) => {
-  res.cookie('refresh-token', token, {
-    expiresIn: 60 * 60 * 24 * 7,
-    httpOnly: true,
-    // sameSite: 'None',
-    // secure: true,
-  })
-}
