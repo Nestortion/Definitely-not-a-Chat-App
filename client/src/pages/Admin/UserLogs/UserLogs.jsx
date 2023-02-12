7252d5d98f1555f5ba3cfdbdbb4f776915c3063f
Josel Catalan <catalanjosel123@gmail.com>
2023-02-12 23:17:54 +0800

    chore: add dropdown on UserLogs

--- client/src/pages/Admin/UserLogs/UserLogs.jsx
+++ client/src/pages/Admin/UserLogs/UserLogs.jsx
@@ -6,6 +6,24 @@ import { useUserLogsQuery } from '../../../graphql/hooks/graphql'
 import InfiniteScroll from 'react-infinite-scroller'
 import { useState } from 'react'
 
+const sections = [
+  {
+    id: 1,
+    name: 'Not Set',
+    disabled: 0,
+  },
+  {
+    id: 2,
+    name: 'BSIT 4-1',
+    disabled: 0,
+  },
+  {
+    id: 3,
+    name: 'DICT 3-1',
+    disabled: 1,
+  },
+]
+
 export default function UserLogs() {
   const {
     data: userLogs,
@@ -33,12 +51,28 @@ export default function UserLogs() {
     })
   }
 
+  const handleChange = (e) => {
+    console.log(e.target.value)
+  }
+
   if (userLogsLoading) return <LoadingSpinner />
   if (userLogsError) return <ErrorText>Something went wrong</ErrorText>
 
   return (
     <div className="user-logs">
       <p className="user-logs__heading fw-bold">User Logs</p>
+      <div className="user-logs__drop-down__container">
+        <label className="fw-bold fs-500" htmlFor="filter-logs">
+          Filter by section:{' '}
+        </label>
+        <select id="filter-logs" onChange={handleChange}>
+          {sections.map((section) => (
+            <option key={section.id} value={section.name}>
+              {section.name}
+            </option>
+          ))}
+        </select>
+      </div>
       <div className="user-logs-container control-panel__card">
         <InfiniteScroll
           pageStart={0}
