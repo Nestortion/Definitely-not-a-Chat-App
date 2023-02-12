53683c3fc29fab961c4af08b1fbb0d19fecc78a8
Nestortion <gerona.nestor@gmail.com>
2023-02-12 17:09:36 +0800

    client: updated hooks for admin logs query

--- client/src/graphql/hooks/gql.ts
+++ client/src/graphql/hooks/gql.ts
@@ -20,7 +20,7 @@ const documents = {
     "mutation AddUserChat($file: Upload, $message: String, $userId: Int, $receiver: Int) {\n  addUserChat(\n    file: $file\n    message: $message\n    user_id: $userId\n    receiver: $receiver\n  ) {\n    message\n    id\n    message_type\n    receiver\n    user_id\n  }\n}": types.AddUserChatDocument,
     "mutation AddUserChatReaction($reaction: String, $count: Int, $userchatId: Int) {\n  addUserChatReaction(\n    reaction: $reaction\n    count: $count\n    userchat_id: $userchatId\n  ) {\n    reaction\n    count\n    userchat_id\n  }\n}": types.AddUserChatReactionDocument,
     "mutation AddUserGroupRole($userGroupId: Int, $groupRoleId: Int) {\n  addUserGroupRole(user_group_id: $userGroupId, group_role_id: $groupRoleId) {\n    group_role_id\n    user_group_id\n  }\n}": types.AddUserGroupRoleDocument,
-    "query AdminLogs {\n  adminLogs {\n    id\n    user_id\n    action_description\n    full_name\n    createdAt\n  }\n}": types.AdminLogsDocument,
+    "query AdminLogs($limit: Int, $offset: Int) {\n  adminLogs(limit: $limit, offset: $offset) {\n    id\n    user_id\n    action_description\n    full_name\n    createdAt\n  }\n}": types.AdminLogsDocument,
     "mutation AdminUpdateUserProfile($userData: AdminUserProfileUpdateInput) {\n  adminUpdateUserProfile(userData: $userData) {\n    id\n    username\n    access_level\n    last_name\n    first_name\n    profile_img\n  }\n}": types.AdminUpdateUserProfileDocument,
     "subscription ChatAdded($user: Int) {\n  chatAdded(user: $user) {\n    id\n    message\n    user_id\n    receiver\n    message_type\n    createdAt\n  }\n}": types.ChatAddedDocument,
     "mutation CreateGroup($userId: [Int!]) {\n  createGroup(user_id: $userId) {\n    group_name\n    group_picture\n    id\n    is_group\n  }\n}": types.CreateGroupDocument,
@@ -100,7 +100,7 @@ export function graphql(source: "mutation AddUserGroupRole($userGroupId: Int, $g
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(source: "query AdminLogs {\n  adminLogs {\n    id\n    user_id\n    action_description\n    full_name\n    createdAt\n  }\n}"): (typeof documents)["query AdminLogs {\n  adminLogs {\n    id\n    user_id\n    action_description\n    full_name\n    createdAt\n  }\n}"];
+export function graphql(source: "query AdminLogs($limit: Int, $offset: Int) {\n  adminLogs(limit: $limit, offset: $offset) {\n    id\n    user_id\n    action_description\n    full_name\n    createdAt\n  }\n}"): (typeof documents)["query AdminLogs($limit: Int, $offset: Int) {\n  adminLogs(limit: $limit, offset: $offset) {\n    id\n    user_id\n    action_description\n    full_name\n    createdAt\n  }\n}"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
